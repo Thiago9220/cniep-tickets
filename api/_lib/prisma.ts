b18ca3fc3c1c7fd18612c5f6e82fdc84c702d2a9
Thiago Ramos <Thiago.ramos356@gmail.com>
2025-12-08 13:44:22 -0300

    fix: client/ts

--- api/_lib/prisma.ts
+++ api/_lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../packages/generated/prisma/client.ts";
+import { PrismaClient } from "../../packages/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
